68e2ad1c8951257773c7d04319463d0f23e8b90e
Horace-Lau <asa_lhs@hotmail.com>
2024-11-21 01:07:14 +0800

    #Feature:初始化项目

---
+++ src/main/java/src/Controller.java
@@ -0,0 +1,12 @@
+package src;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+    @RequestMapping("/hello")
+    public String sayHello(){
+        return "Hello World";
+    }
+}
